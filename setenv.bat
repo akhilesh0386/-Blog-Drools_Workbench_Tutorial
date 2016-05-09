04dc7c7a73ba7f1ed28c3d268aca28eafd7272a1
I322871 <wangcong1221@gmail.com>
2016-05-09 15:32:23 +0800

    'Blog'

---
+++ setenv.bat
@@ -0,0 +1,1 @@
+set CATALINA_OPTS=-Xmx512M -XX:MaxPermSize=512m -Dbtm.root=%CATALINA_HOME% -Dbitronix.tm.configuration=%CATALINA_HOME%\conf\btm-config.properties -Djbpm.tsr.jndi.lookup=java:comp/env/TransactionSynchronizationRegistry -Djava.security.auth.login.config=%CATALINA_HOME%\webapps\drools-wb\WEB-INF\classes\login.config -Dorg.jboss.logging.provider=jdk
